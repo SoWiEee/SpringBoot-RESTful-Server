f4b61cbd1f359e39d3b2d09587b8c6f3616b664b
SoWiEee <kawiisowieee@gmail.com>
2024-12-02 17:47:43 +0800

    project init

---
+++ src/main/java/org/example/springnewbie/SpringNewbieApplication.java
@@ -0,0 +1,13 @@
+package org.example.springnewbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewbieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNewbieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springnewbie/SpringNewbieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springnewbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewbieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
