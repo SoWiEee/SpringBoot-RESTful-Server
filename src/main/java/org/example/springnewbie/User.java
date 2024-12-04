52e43dc36b74e77fe148ea0384add250d537a037
SoWiEee <kawiisowieee@gmail.com>
2024-12-04 23:46:15 +0800

    add User class

---
+++ src/main/java/org/example/springnewbie/User.java
@@ -0,0 +1,28 @@
+package org.example.springnewbie;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class User {
+    public String name;
+    public String email;
+    public String passwd;     // MD5
+    public User(String name, String email, String passwd) {
+        this.name = name;
+        this.email = email;
+        this.passwd = passwd;
+    }
+
+    public static String hashPasswd(String passwd) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(passwd.getBytes());
+        byte[] digest = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for(byte b : digest){
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
