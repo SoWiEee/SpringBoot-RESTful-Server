a58cf97645422c30ad195dd7ef8445e03b3e4fad
SoWiEee <kawiisowieee@gmail.com>
2024-12-08 12:29:15 +0800

    add API_ref page

--- src/main/java/org/example/springnewbie/SpringNewbieApplication.java
+++ src/main/java/org/example/springnewbie/SpringNewbieApplication.java
@@ -3,11 +3,10 @@ package org.example.springnewbie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// annotation - Spring Boot Entry
 @SpringBootApplication
 public class SpringNewbieApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringNewbieApplication.class, args);
     }
-
 }
