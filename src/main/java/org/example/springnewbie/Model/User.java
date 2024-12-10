2927ba8f48d55dd823574988fc9021fbec5ae6b7
SoWiEee <kawiisowieee@gmail.com>
2024-12-10 21:17:47 +0800

    modify variable name

--- src/main/java/org/example/springnewbie/Model/User.java
+++ src/main/java/org/example/springnewbie/Model/User.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
 
 public class User {
 
@@ -14,15 +13,18 @@ public class User {
     @Email
     public String email;
     @NotBlank
-    public String passwd;     // SHA-256
+    public String password;     // SHA-256
 
     // getter & setter
     public String getEmail() { return this.email; }
     public String getName() { return this.name; }
-    public String getPasswd() { return this.passwd; }
+    public String getPasswd() { return this.password; }
+    public void setEmail(String email) { this.email = email; }
+    public void setName(String name) { this.name = name; }
+    public void setPasswd(String password) { this.password = password; }
 
     // use SHA256 hashing
-    public static String hashPasswd(String passwd) throws NoSuchAlgorithmException {
+    public static String hashPasswd(String passwd) {
         return  Hashing.sha256().hashString(passwd, StandardCharsets.UTF_8).toString();
     }
 }
