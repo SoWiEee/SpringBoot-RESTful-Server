c5a5e639cf36994231c28c02488205ff17741506
SoWiEee <kawiisowieee@gmail.com>
2024-12-11 19:23:19 +0800

    disable JacksonAuto parse

--- src/main/java/org/example/springnewbie/SpringNewbieApplication.java
+++ src/main/java/org/example/springnewbie/SpringNewbieApplication.java
@@ -2,9 +2,10 @@ package org.example.springnewbie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 // annotation - Spring Boot Entry
-@SpringBootApplication
+@SpringBootApplication(exclude = { JacksonAutoConfiguration.class })
 public class SpringNewbieApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringNewbieApplication.class, args);
