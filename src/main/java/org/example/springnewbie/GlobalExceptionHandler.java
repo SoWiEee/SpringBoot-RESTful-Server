35ed44030f450ebb4e36543558cda01eca41d35b
SoWiEee <kawiisowieee@gmail.com>
2024-12-15 13:06:31 +0800

    send PARAMS_MISSING response

--- src/main/java/org/example/springnewbie/GlobalExceptionHandler.java
+++ src/main/java/org/example/springnewbie/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package org.example.springnewbie;
 
+import org.example.springnewbie.RspDTO.Common_Rsp;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,15 +19,22 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex){
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(FieldError::getDefaultMessage)
-                .collect(Collectors.toList());
+    private static final Gson GSON = new Gson();
 
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleValidationErrors(MethodArgumentNotValidException ex){
+//        List<String> errors = ex.getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(FieldError::getDefaultMessage)
+//                .collect(Collectors.toList());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        Common_Rsp rsp = new Common_Rsp();
+        rsp.PARAMS_MISSING();
+
+        return new ResponseEntity<>(GSON.toJson(rsp), headers, HttpStatus.BAD_REQUEST);
     }
 
     private Map<String, List<String>> getErrorsMap(List<String> errors) {
