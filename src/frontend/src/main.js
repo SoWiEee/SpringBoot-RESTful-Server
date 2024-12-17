d40909773c3c3b60756721412842c4938aa315d8
SoWiEee <kawiisowieee@gmail.com>
2024-12-17 11:40:39 +0800

    add vuetify

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,10 +3,19 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const app = createApp(App);
 
 app.config.globalProperties.$http = axios;
 
-app.use(router).mount('#app');
+app.use(router).use(vuetify).mount('#app');
 
