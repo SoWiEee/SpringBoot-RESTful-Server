2b9530d30b1bc87a13b768b00d26557a750a95f0
SoWiEee <kawiisowieee@gmail.com>
2024-12-21 15:59:49 +0800

    update validateToken

--- src/main/java/org/example/springnewbie/JwtToken.java
+++ src/main/java/org/example/springnewbie/JwtToken.java
@@ -1,5 +1,6 @@
 package org.example.springnewbie;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,7 @@ import java.util.Date;
 public class JwtToken {
     private static final String ISS = "P3K0p3K0";
     private static final long EXPIRE_TIME = 60 * 60 * 1000;
-    SecretKey key = Jwts.SIG.HS256.key().build();
+    SecretKey key = Jwts.SIG.HS512.key().build();
 
     public String generateToken(String username) {
         return Jwts.builder()
@@ -19,5 +20,21 @@ public class JwtToken {
                 .expiration(new Date(System.currentTimeMillis() + EXPIRE_TIME))
                 .signWith(key)
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        boolean valid = true;
+
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+        } catch (JwtException ex) {
+            valid = false;
+        }
+
+        return valid;
     }
 }
