f12fe9edd3bd20d669e52149bb3ee484234437b4
SoWiEee <kawiisowieee@gmail.com>
2024-12-21 15:39:58 +0800

    add generateToken

--- src/main/java/org/example/springnewbie/JwtToken.java
+++ src/main/java/org/example/springnewbie/JwtToken.java
@@ -5,9 +5,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Claims;
-import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 
 @Component
